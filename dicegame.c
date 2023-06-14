3209eb10716992b438d91225c62998be075b19a4
s1300010 <136044553+s1300010@users.noreply.github.com>
2023-06-14 13:48:21 +0900

    Won/lost message modified

--- dicegame.c
+++ dicegame.c
@@ -5,6 +5,13 @@
 int main() {
     int die1, die2, total;
     char name[40];
+    
+    
+    printf("What is your name?\n");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+    
+    char name[40];
 
 
     srand(time(NULL));
