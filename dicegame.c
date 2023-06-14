46d392be04ff00020871a5da23a8e6d87ce57248
s1300010 <136044553+s1300010@users.noreply.github.com>
2023-06-14 13:22:47 +0900

    Won/lost message added

--- dicegame.c
+++ dicegame.c
@@ -4,6 +4,8 @@
 
 int main() {
     int die1, die2, total;
+    char name[40];
+
 
     srand(time(NULL));
 
@@ -15,6 +17,13 @@ int main() {
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
     printf("Total value: %d\n", total);
+
+
+    if (total > 7) {
+            printf("%s won!\n", name);
+        } else {
+            printf("%s lost.\n", name);
+        }
 
     return 0;
 }
